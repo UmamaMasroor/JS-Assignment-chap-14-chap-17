404c47e16c89111f339ae9f7b9ff4632150cf991
Umama Masroor <umamafatima319@gmail.com>
2023-05-17 20:05:35 -0700

    question 2

---
+++ question # 2/add.js
@@ -0,0 +1,8 @@
+// question 2
+// Declare an empty array using JS object notation to store
+// student names in future.
+let names= {
+    "student1" : "",
+    "student2" : "",
+    "student3" : ""
+    }
